99accd7ed3623f6d858ed5cdec732b3ea9f7f4f3
trzeciakp20 <m.trzeciak.p20@gmail.com>
2022-07-16 15:33:44 +0200

    structure change

--- js/main.js
+++ js/main.js
@@ -4,25 +4,24 @@ let board = [new Array(8), new Array(8), new Array(8), new Array(8), new Array(8
 let made_moves = [new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8), new Array(8)]
 let previous_tile = [null, null]
 
-for(let i = 0; i < 8; i++){     //putting tiles references into board table    
+for(let i = 0; i < 8; i++){         
     for(let j = 0; j < 8; j++){
-        board[i][j] = document.querySelectorAll('main .row')[i].querySelectorAll('.tile')[j]
-        
-        board[i][j].addEventListener("click", () =>{
+        board[i][j] = document.querySelectorAll('main .row')[i].querySelectorAll('.tile')[j]   //putting tiles references into board table    
+        board[i][j].addEventListener("click", () =>{        //adding them events
             movePiece(i, j)
-
         })
-    }
-    
+    } 
 }
+
+
 
 function movePiece(i, j){
     console.log(i, j)
     if(previous_tile[0] != null){          //wywalenie poprzedniej figury
         board[previous_tile[i]][previous_tile[j]].style.backgroundImage = "none"
     }
-    // board[i][j].style.backgroundImage = "ae"
+    board[i][j].style.backgroundImage = 
 
-    // previous_tile = [i, j]
+    previous_tile = [i, j]
 }
 
