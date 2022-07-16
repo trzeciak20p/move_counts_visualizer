c29c301d52985f3c30475fc23abb414b09258480
trzeciakp20 <m.trzeciak.p20@gmail.com>
2022-07-16 15:14:06 +0200

    stroeing some stupid shit (may be useful or  so)

--- main.js
+++ main.js
@@ -7,21 +7,19 @@ let previous_tile = [null, null]
 for(i = 0; i < 8; i++){     //putting tiles references into board table
     for(j = 0; j < 8; j++){
         board[i][j] = document.querySelectorAll('main .row')[i].querySelectorAll('.tile')[j]
-        board[i][j].addEventListener("click", (i, j) =>{
-            console.log(i)
-            console.log(j)
-
-            // movePiece(i, j)
+        var x = i
+        board[i][j].addEventListener("click", () =>{
+            movePiece(i, j)
         })
     }
 }
 
 function movePiece(i, j){
-    console.log(i,j)
-    if(previous_tile[0] != null){          //wywalenie poprzedniego pionka
+    console.log(i, j)
+    if(previous_tile[0] != null){          //wywalenie poprzedniej figury
         board[previous_tile[i]][previous_tile[j]].style.backgroundImage = "none"
     }
-    board[i][j].style.backgroundImage = "ae"
+    // board[i][j].style.backgroundImage = "ae"
 
 
 }
